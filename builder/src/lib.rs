19bdf8681753e699bdca742df99d53941c81d03e
ivicel <31336040+ivicel@users.noreply.github.com>
2023-11-26 16:53:10 +0800

    builder 03

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,6 +1,6 @@
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, spanned::Spanned, DeriveInput, Ident, Result};
+use syn::{parse_macro_input, spanned::Spanned, DeriveInput, Ident, Result, Type};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
@@ -32,6 +32,7 @@ fn generate_builder(input: &DeriveInput) -> Result<proc_macro2::TokenStream> {
 }
 
 fn generate_builder_struct(input: &DeriveInput) -> Result<proc_macro2::TokenStream> {
+    // builder struct 名称
     let builder_ident = get_builder_struct_ident(input);
 
     // 判断只能使用在 struct 上
@@ -43,13 +44,17 @@ fn generate_builder_struct(input: &DeriveInput) -> Result<proc_macro2::TokenStre
         _ => return Err(syn::Error::new(input.span(), "Only use on struct")),
     };
 
+    // struct 字段名
     let mut fields_ident = Vec::new();
+    // struct 字段类型
     let mut fields_ty = Vec::new();
 
     for field in fields.named.iter() {
         fields_ident.push(field.ident.as_ref().unwrap());
         fields_ty.push(&field.ty);
     }
+
+    let setter_fns = generate_setter_fn(&fields_ident, &fields_ty)?;
 
     let ret = quote! {
         pub struct #builder_ident {
@@ -62,10 +67,31 @@ fn generate_builder_struct(input: &DeriveInput) -> Result<proc_macro2::TokenStre
                     #(#fields_ident: core::option::Option::None),*
                 }
             }
+
+            #(#setter_fns)*
         }
     };
 
     Ok(ret)
+}
+
+fn generate_setter_fn<'a>(
+    fields_ident: &'a [&Ident],
+    fields_ty: &'a [&Type],
+) -> Result<Vec<proc_macro2::TokenStream>> {
+    let mut setter_fns = Vec::new();
+    for (ident, ty) in fields_ident.iter().zip(fields_ty) {
+        let setter_fn = quote! {
+            pub fn #ident(&mut self, #ident: #ty) -> &mut Self {
+                self.#ident = core::option::Option::Some(#ident);
+                self
+            }
+        };
+
+        setter_fns.push(setter_fn);
+    }
+
+    Ok(setter_fns)
 }
 
 fn get_builder_struct_ident(input: &DeriveInput) -> Ident {
